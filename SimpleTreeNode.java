cbd23a691f96cb3675f9c9cd2c842410ac93a2bc
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-07-28 01:29:50 +0300

    Update SimpleTreeNode.java

--- SimpleTreeNode.java
+++ SimpleTreeNode.java
@@ -23,9 +23,10 @@ class SimpleTree<T>
     {
         Root = root;
     }
+
 //--------------------------------------------------------------------------------------
     public void AddChild (SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild)
-    { // ваш код добавления нового дочернего узла существующему ParentNode
+    { 
         if (ParentNode == null || NewChild == null) {
             throw new NullPointerException  ("\n Обращение к нулевому параметру \n");
         }
@@ -35,9 +36,10 @@ class SimpleTree<T>
          ParentNode.Children.add(NewChild);
          NewChild.Parent = ParentNode;
     }
+
 //----------------------------------------------------------------------------------------
     public void DeleteNode(SimpleTreeNode<T> NodeToDelete)
-    { // ваш код удаления существующего узла NodeToDelete
+    { 
         if (NodeToDelete == null)
             return;
 
@@ -66,14 +68,11 @@ class SimpleTree<T>
         }
         NodeToDelete.Parent.Children.addAll(NodeToDelete.Children);
         NodeToDelete.Parent.Children.remove(NodeToDelete);
-
-        //List nodes = this.GetAllNodes();
-          //  nodes.remove(NodeToDelete);
     }
 
 //----------------------------------------------------------------------------------------
     public List<SimpleTreeNode<T>> GetAllNodes()
-    {           // ваш код выдачи всех узлов дерева в определённом порядке
+    {       
         List<SimpleTreeNode<T>> nodeList = new LinkedList<SimpleTreeNode<T>>();
         if (this.Root == null)
             return nodeList;
@@ -104,7 +103,7 @@ class SimpleTree<T>
 
 //--------------------------------------------------------------------------------------
     public List<SimpleTreeNode<T>> FindNodesByValue (T val)
-    {       // ваш код поиска узлов по значению
+    {    
         List<SimpleTreeNode<T>>  resultValList = new LinkedList<SimpleTreeNode<T>>();
 
         if (this.Root == null)
@@ -117,7 +116,7 @@ class SimpleTree<T>
          return  resultValList;
     }
 
- //------
+ //--
     public List<SimpleTreeNode<T>>  SubListOfValue (SimpleTreeNode<T> node, T val ) {
         List<SimpleTreeNode<T>> resList  = new LinkedList<SimpleTreeNode<T>>();
 
@@ -137,9 +136,9 @@ class SimpleTree<T>
         return resList;
     }
 
-    //--------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------
     public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent)
-    {  // ваш код перемещения узла вместе с его поддеревом в качестве дочернего для узла NewParent
+    {  
         if (NewParent == null || OriginalNode == null)
             return;
 
@@ -152,14 +151,14 @@ class SimpleTree<T>
 //---------------------------------------------------------------------
 
     public int Count()
-    { // количество всех узлов в дереве
+    { 
         List<SimpleTreeNode<T>>  nodeList = GetAllNodes();
         return nodeList.size();
     }
 //--------------------------------------------------------------------------
 
     public int LeafCount()
-    { // количество листьев в дереве
+    { 
         if (this.Root.Children == null)
             return 0;
 
@@ -173,61 +172,11 @@ class SimpleTree<T>
         }
         return LeafCount;
     }
-//---------------------------------------------------------------------------
 
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        SimpleTreeNode<Integer>  myRoot = new SimpleTreeNode(1, null);
+    
+    
+} 
 
-        SimpleTree<Integer> myTree = new SimpleTree<Integer> (myRoot);
 
-        SimpleTreeNode<Integer> ch2 = new SimpleTreeNode<Integer>(2, myRoot);
-        SimpleTreeNode<Integer> ch3 = new SimpleTreeNode<Integer>(3, myRoot);
-        SimpleTreeNode<Integer> ch4 = new SimpleTreeNode<Integer>(4, myRoot);
-        SimpleTreeNode<Integer> ch5 = new SimpleTreeNode<Integer>(5, myRoot);
-        SimpleTreeNode<Integer> ch6 = new SimpleTreeNode<Integer>(6, myRoot);
-        SimpleTreeNode<Integer> ch7 = new SimpleTreeNode<Integer>(7, myRoot);
 
-        SimpleTreeNode<Integer> ch8 = new SimpleTreeNode<Integer>(8, myRoot);
-        SimpleTreeNode<Integer> ch9 = new SimpleTreeNode<Integer>(9, myRoot);
-        SimpleTreeNode<Integer> ch10 = new SimpleTreeNode<Integer>(10, myRoot);
-        SimpleTreeNode<Integer> ch11 = new SimpleTreeNode<Integer>(11, myRoot);
 
-        myTree.AddChild(myRoot, ch2);
-        myTree.AddChild(myRoot, ch3);
-
-        myTree.AddChild(ch2, ch4);
-        myTree.AddChild(ch2, ch5);
-
-        myTree.AddChild(ch4, ch8);
-        myTree.AddChild(ch4, ch9);
-
-        myTree.AddChild(ch3, ch6);
-        myTree.AddChild(ch3, ch7);
-
-        myTree.AddChild(ch5, ch10);
-        myTree.AddChild(ch10, ch11);
-
-        myTree.DeleteNode(ch11);
-        myTree.DeleteNode(ch4);
-
-        List<SimpleTreeNode<Integer>> allNodes = myTree.GetAllNodes();
-        List<SimpleTreeNode<Integer>> allVal = myTree.FindNodesByValue(11);
-        int nodeCount = myTree.Count();
-        int leafCount = myTree.LeafCount();
-
-        myTree.DeleteNode(myTree.Root);
-
-        for (int i = 0; i < allVal.size(); i++) {
-            SimpleTreeNode ch = allVal.get(i);
-
-            System.out.printf("\n val =  %d", ch.NodeValue);
-        }
-
-        for (int i = 0; i < allNodes.size(); i++) {
-                SimpleTreeNode ch = allNodes.get(i);
-
-                System.out.printf("\n val =  %d", ch.NodeValue);
-        }
-    }
-}
