fd9abfcaf6a67d8349f455058bf141c8cab8a814
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-08-08 02:51:39 +0300

    Update and rename BSTNode2.java to BSTNode-2.java

--- BSTTarea3/BSTNode-2.java
+++ BSTTarea3/BSTNode-2.java
@@ -1,3 +1,5 @@
+//  Заданиe 2 задачи 1, 2 и 3
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,15 +45,18 @@ class BST<T>
         Root = node;
     }
 
-    
-//=======Метод который проверяет идентичность двух бинарных деревьев================
- 
+//=====================================================================
+//  Задание 2   Задача 1
+// Метод который проверяет идентичность двух бинарных деревье
+// сложность O(N) и пространственная и временная 
+//==========================================================    
+
     public boolean CompareBinTrees (BST binTree2)
     {
         return CompareAllNodes ( binTree2.Root, this.Root);
     }
     
-    //--------------------------
+    //--
     boolean CompareAllNodes (BSTNode2<T> node1, BSTNode2<T> node2)
     {
         if (node1 == null && node2 == null)
@@ -70,8 +75,11 @@ class BST<T>
                 CompareAllNodes(node1.RightChild, node2.RightChild);
         }
 
-
-//=====метод, который нахождит все пути от корня к листьям, заданной длины.
+//=====================================================================
+//  Задание 2   Задача 2
+// Метод, который нахождит все пути от корня к листьям, заданной длины
+// сложность O(N) и пространственная и временная 
+//==========================================================   
 
     List<Integer> waysofLen (BSTNode2 node, int len, int n) {
         List<Integer> sList = new ArrayList<>();
@@ -84,27 +92,27 @@ class BST<T>
             sList.add(n);
             return sList;
         }
-
-        System.out.printf(" n = %d , key =  % d \n",  n, node.NodeKey);
-
-        // для левого а потом правого
+        // левая а потом правая
         int m = n+1;
         sList.addAll(waysofLen (node.LeftChild, len, m));
         sList.addAll(waysofLen (node.RightChild, len, m));
         return sList;
     } 
 
-//------------------------------------------------
+//---------------------------------------------------------------------
     public boolean isLeaf (BSTNode2 node) {
         if (node != null && node.RightChild == null && node.RightChild == null)
             return true;
-
-        return false;
+      return false;
     }
+//-----------------------------------------------------------------------
 
-
-    //=====метод, который находит все пути от корня к листьям, с мах суммой значений узлов
-    
+//=====================================================================
+//  Задание 2   Задача 3
+// Метод, который находит все пути от корня к листьям, с мах суммой значений узлов
+// сложность O(N) и пространственная и временная 
+//======================================================================      
+ 
     List <Integer> sumNodeWays (BSTNode2 node, int k) {
         List<Integer> sList = new ArrayList<>();
 
@@ -118,11 +126,14 @@ class BST<T>
         if (isLeaf (node)) {
             return sList;
         }
-        // для левого а потом правого
+        // для левой а потом правой
         sList.addAll (sumNodeWays(node.LeftChild,  k));
         sList.addAll (sumNodeWays(node.RightChild, k));
         return sList;
     }
+//=============================================================================
+
+} 
 
 
-}
+
