64477bd1e51d530c23c1a4c11a58b3270ab0e8b0
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-08-22 17:08:11 +0300

    Update AlgorithmsDataStructures2.java

--- AlgorithmsDataStructures2.java
+++ AlgorithmsDataStructures2.java
@@ -4,8 +4,8 @@ public class AlgorithmsDataStructures2 {
 
     public static int[] GenerateBBSTArray(int[] a) {
         int len = a.length;
-        //Sort
-        Arrays.sort(a);
+       
+        Arrays.sort(a);  //Sort
 
         int b[] = new int[len];
 
@@ -21,21 +21,19 @@ public class AlgorithmsDataStructures2 {
         int mid = (int)((left+right) / 2 );
         int val = A [mid];
 
-        if (pos > B.length-1)
+        if (pos > B.length - 1)
             return B;
         B[pos] = val;
 
 
-        makeBSTArray (left, mid -1, pos*2+1, A, B);
-        makeBSTArray (mid +1, right, pos*2+2, A, B);
+        makeBSTArray (left, mid - 1, pos*2 + 1, A, B);
+        makeBSTArray (mid + 1, right, pos*2 + 2, A, B);
 
         return B;
     }
 
 
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+ }
 
-    }
-}
+
 
