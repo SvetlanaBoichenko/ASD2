37b8508a2137a2e13263b3e5531769ad37665d2c
SvetlanaBoichenko <160069590+SvetlanaBoichenko@users.noreply.github.com>
2025-08-28 13:48:17 +0300

    Update BalancedBST.java

--- BalancedBST.java
+++ BalancedBST.java
@@ -1,13 +1,12 @@
-
 import java.util.*;
 
 class BSTNode
 {
     public int NodeKey; //
-    public BSTNode Parent; // 
-    public BSTNode LeftChild; // 
-    public BSTNode RightChild; // 
-    public int     Level; // 
+    public BSTNode Parent; //
+    public BSTNode LeftChild; //
+    public BSTNode RightChild; //
+    public int     Level; //
 
     public BSTNode(int key, BSTNode parent)
     {
@@ -29,31 +28,31 @@ class BalancedBST
 
     public void GenerateTree(int[] a)
     {
-    
+
         // 1. Sort
         int len = a.length;
         //Sort
         Arrays.sort(a);
 
-        BSTNode b[] = new BSTNode[len];   
+        BSTNode b[] = new BSTNode[len];
         // 2. nodes
-        makeBSTArray (0, len-1, 0, a,  null, 0); // 
+        makeBSTArray (0, len-1,  a,  null, 0); //
     }
 
 
-    public  BSTNode makeBSTArray (int left, int right, int pos, int[] A,  BSTNode parent, int level )
+    public  BSTNode makeBSTArray (int left, int right, int[] A,  BSTNode parent, int level )
     {
-       
+
         int mid = (int)((left+right) / 2 );
         int val = A [mid];
         //  node
         BSTNode new_nod = new BSTNode (val, parent);
         new_nod.Level = level;  //(int)(Math.log(node_count) / Math.log(2));;
-      
-       if (right  > left) {
+
+        if (right  > left) {
             new_nod.LeftChild = makeBSTArray(left, mid - 1, A, new_nod, level + 1); //
-            new_nod.RightChild = makeBSTArray(mid + 1, right, A, new_nod, level + 1); 
-       }
+            new_nod.RightChild = makeBSTArray(mid + 1, right, A, new_nod, level + 1);
+        }
 
         return new_nod;
     }
@@ -61,8 +60,8 @@ class BalancedBST
 
     public boolean IsBalanced(BSTNode root_node)
     {
-      int r = difLevels(root_node);
-       if ( r < 2)
+        int r = difLevels(root_node);
+        if ( r < 2)
             return true; //
 
         return false;
@@ -73,18 +72,18 @@ class BalancedBST
         if (parent_node == null) {
             return -1;
         }
-// necesita extacto left and right
+    // necesita extacto left and right
         int left_level = difLevels(parent_node.LeftChild);
         int right_level = difLevels(parent_node.RightChild);
- 
+
         int ret = Math.abs(left_level - right_level);
 
         return ret;
     }
 
-    
 
 }
+
 
 
 
